880b94f8ccab5a77fef74d2b4135791e88ad55b3
BDWalker23 <BDWalker23@gmail.com>
2025-07-15 14:06:30 -0400

    Add files via upload

--- calendar_pdf_2.py
+++ calendar_pdf_2.py
@@ -29,7 +29,7 @@ class CalendarPDF(FPDF):
         # Weekday headers
         self.set_font("Arial", "B", 10)
         cell_width = 40
-        cell_height = 60
+        cell_height = 75  # Tripled height
         days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
         for day in days:
             self.cell(cell_width, 10, day, border=1, align="C")
@@ -44,21 +44,21 @@ class CalendarPDF(FPDF):
                 y = self.get_y()
                 self.rect(x, y, cell_width, cell_height)
                 if day.month == month:
-                    self.set_xy(x + 1, y + 1)
-                    self.set_font("Arial", "B", 9)
+                    self.set_xy(x + 1, y + 2)
+                    self.set_font("Arial", "B", 10)
                     self.set_text_color(0)
-                    self.cell(cell_width - 2, 5, str(day.day), ln=1)
+                    self.cell(cell_width - 2, 6, str(day.day), ln=1)
 
                     if day in schedule:
                         name = schedule[day]
                         others = [n for n in ["Brandon", "Tony", "Erik"] if n != name]
                         r, g, b = get_color(name)
-                        self.set_font("Arial", "", 9)
-                        self.set_xy(x + 1, y + 7)
+                        self.set_font("Arial", "", 10)
+                        self.set_xy(x + 1, y + 10)
                         self.set_text_color(r, g, b)
-                        self.cell(cell_width - 2, 5, f"{name} OFF", ln=1)
-                        self.set_xy(x + 1, y + 13)
+                        self.cell(cell_width - 2, 6, f"{name} OFF", ln=1)
+                        self.set_xy(x + 1, y + 18)
                         self.set_text_color(0)
-                        self.cell(cell_width - 2, 5, f"{others[0]} & {others[1]} ON", ln=1)
+                        self.cell(cell_width - 2, 6, f"{others[0]} & {others[1]} ON", ln=1)
                 self.set_xy(x + cell_width, y)
             self.ln()
