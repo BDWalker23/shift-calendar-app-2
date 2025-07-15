f3eb19c11395c2a32cdecbee33fa9568d4cabf8d
BDWalker23 <BDWalker23@gmail.com>
2025-07-15 13:58:33 -0400

    Update autopopulate_schedule_2.py

--- autopopulate_schedule_2.py
+++ autopopulate_schedule_2.py
@@ -1,6 +1,7 @@
 import calendar
 from datetime import date
 from collections import Counter
+from datetime import timedelta
 
 def autopopulate_schedule(year, month, initial_schedule):
     all_days = [date(year, month, d) for d in range(1, calendar.monthrange(year, month)[1] + 1)]
